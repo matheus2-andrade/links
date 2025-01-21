85b2ebd53bf3ab3e0513c8c1020c23bbdf6231fd
matheusandrade <mthrafaelandrade@gmail.com>
2025-01-21 10:49:24 -0300

    estrutura inicial criada

---
+++ src/app/index/index.tsx
@@ -0,0 +1,19 @@
+import { Image, View, TouchableOpacity } from 'react-native'
+import { MaterialIcons } from "@expo/vector-icons"
+
+import { styles } from './styles'
+import { colors } from '@/styles/colors'
+
+export default function Index(){
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={require("@/assets/logo.png")} style={styles.logo}></Image>
+                <TouchableOpacity>
+                    <MaterialIcons name="add" size={32} color={colors.green[300]}></MaterialIcons>
+                </TouchableOpacity>
+               
+            </View>
+        </View>
+    )
+}
